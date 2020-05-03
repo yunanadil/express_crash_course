d4f6a25e28ddf81c24c306b1bdff289f8dd41f77
yunanadil <muhammadyunanadil@gmail.com>
2020-05-03 20:31:04 +0800

    Initial Commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+	{
+		id: 1,
+		name: 'Taylor Swift',
+		email: 'taylorswift@gmail.com',
+		status: 'active',
+	},
+	{
+		id: 2,
+		name: 'Marshall Mathers',
+		email: 'slimshady@gmail.com',
+		status: 'inactive',
+	},
+	{
+		id: 3,
+		name: 'Hayley Williams',
+		email: 'hayleywilliams@gmail.com',
+		status: 'active',
+	},
+];
+
+module.exports = members;
